a05bdefc0aeaa0ee0920eacd7bb8114b546342ef
Susuper2019 <gjd2019@126.com>
2019-08-11 11:38:19 +0800

    test4

--- BCS/src/cn/bcs/bean/User.java
+++ BCS/src/cn/bcs/bean/User.java
@@ -4,4 +4,18 @@ public class User {
 	private Integer id;
 	private String user;
 	private String password;
+	
+	
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", user=" + user + ", password=" + password + "]";
+	}
+
+
+	public static void main(String[] args) {
+		User user = new User();
+		user.id = 1;
+		System.out.println(user);
+	}
+	    
 }
