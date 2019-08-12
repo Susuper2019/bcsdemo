295156db44989083bac7ba5dd35a81d3f14bb842
Susuper2019 <gjd2019@126.com>
2019-08-12 21:41:45 +0800

    111

---
+++ BCS/src/cn/bcs/dao/Test.java
@@ -0,0 +1,7 @@
+package cn.bcs.dao;
+
+public class Test {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ BCS/src/cn/bcs/service/Test.java
@@ -0,0 +1,10 @@
+package cn.bcs.service;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
